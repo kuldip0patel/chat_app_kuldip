b412986674be0aed738ca49197a09e65659a530b
kuldip-baya <kuldip@baya.biz>
2025-02-27 17:24:09 +0530

    (infra): Fixing UI deployment on render.com

--- frontend/chatapp/next.config.ts
+++ frontend/chatapp/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false 
-  /* config options here */
+  devIndicators: false ,
+  output: 'export', // static export
+  images: {
+    unoptimized: true, // unoptimise
+  },
 };
 
 export default nextConfig;
